c2fa87b170e50742880117da4280d0f024b92c4b
Adil Jeddi Radja <radjaajr@gmail.com>
2024-08-22 19:24:30 +0700

    [fixing]

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/adiljeddiradja/Portofolio-Web
+                href="https://github.com/adiljeddiradja/adiljeddiradja-portofolio
                 "
                 target="_blank"
                 className="fork-btn-inner"
