93f792577049055610cc98a5512b4b5e9d182a45
Adil Jeddi Radja <radjaajr@gmail.com>
2024-08-27 00:56:20 +0700

    [fixing]

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -25,7 +25,7 @@ function Home2() {
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Java. </b>
+                <b className="purple"> C#, Javascript and Java. </b>
               </i>
               <br />
               <br />
